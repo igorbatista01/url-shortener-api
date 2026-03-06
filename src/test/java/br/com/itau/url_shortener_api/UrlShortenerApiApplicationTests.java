157d9c286e06bb6d8999ff20bc0bf5d3a1b6e30b
Igor Batista <contato@igorpereira.com.br>
2026-03-05 22:36:51 -0300

    Fundation

---
+++ src/test/java/br/com/itau/url_shortener_api/UrlShortenerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.url_shortener_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
